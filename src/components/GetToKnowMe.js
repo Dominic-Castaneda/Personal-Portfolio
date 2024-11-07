7e4bb2a552bc97e3f25b4a360ae75b9f46f4d972
Dominic Castaneda <133822774+Dominic-Castaneda@users.noreply.github.com>
2024-11-07 17:21:47 -0500

    Update

--- src/components/GetToKnowMe.js
+++ src/components/GetToKnowMe.js
@@ -8,7 +8,7 @@ const GetToKnowMe = () => {
       <p>Here are some glimpses of my life beyond coding. These pictures capture my hobbies, travels, and special moments that are important to me.</p>
       <div className="pictures-row">
         <img src="/images/hobby1.jpg" alt="Hobby 1" className="get-to-know-me-image" />
-        <img src="/images/hobby2.jpg" alt="Hobby 2" className="get-to-know-me-image" />
+        <img src="/images/bee-frames.jpg" alt="Hobby 2" className="get-to-know-me-image" />
         <img src="/images/hobby3.jpg" alt="Hobby 3" className="get-to-know-me-image" />
         <img src="/images/hobby4.jpg" alt="Hobby 4" className="get-to-know-me-image" />
       </div>
